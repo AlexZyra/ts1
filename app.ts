ed5cb01de3f68505945ff2ffe564f665d214891e
Alex Zyra <117516145+AlexZyra@users.noreply.github.com>
2023-11-17 12:19:37 +0200

    dz 8 On branch main Your branch is up to date with 'nothing-special/main'.
    Changes to be committed:
    	modified:   app.js
    	modified:   app.ts

--- app.ts
+++ app.ts
@@ -681,60 +681,204 @@
 //     }
 // }
 
-const memoizeMap = new Map<string, any>();
+// const memoizeMap = new Map<string, any>();
 
-function Memoize(key: string, descriptor: PropertyDescriptor) {
-    const originalMethod = descriptor.value!;
+// function Memoize(key: string, descriptor: PropertyDescriptor) {
+//     const originalMethod = descriptor.value!;
 
-    descriptor.value = function (this: any, ...args: any[]) {
-        const memoizeKey = `${key}_${JSON.stringify(args)}`;
+//     descriptor.value = function (this: any, ...args: any[]) {
+//         const memoizeKey = `${key}_${JSON.stringify(args)}`;
 
-        if (memoizeMap.has(memoizeKey)) {
-            console.log('Memoized result retrieved:', memoizeMap.get(memoizeKey));
+//         if (memoizeMap.has(memoizeKey)) {
+//             console.log('Memoized result retrieved:', memoizeMap.get(memoizeKey));
+//         } else {
+//             const result = originalMethod.apply(this, args);
+//             memoizeMap.set(memoizeKey, result);
+//             console.log('Method executed:', result);
+//         }
+//     };
+
+//     return descriptor;
+// }
+
+// class MemoizeClass {
+//     @Memoize
+//     calculate(arg1: number, arg2: string): string {
+//         return `Result: ${arg1} - ${arg2}`;
+//     }
+// }
+
+// const instanceMemoize = new MemoizeClass();
+
+// function Debounce(wait: number) {
+//     return function (_target: any, _key: string, descriptor: PropertyDescriptor) {
+//       const originalMethod = descriptor.value!;
+//       let timeout: number | undefined;
+
+//       descriptor.value = function (this: any, ...args: any[]) {
+//         if (timeout !== undefined) {
+//           clearTimeout(timeout);
+//         }
+
+//         timeout = window.setTimeout(() => {
+//           originalMethod.apply(this, args);
+//         }, wait);
+//       };
+
+//       return descriptor;
+//     };
+//   }
+
+//   class DebounceClass {
+//     @Debounce(500)
+//     methodWithDebounce(arg: string) {
+//       console.log(`Method executed with argument: ${arg}`);
+//     }
+//   }
+
+//   const instanceDebounce = new DebounceClass();
+
+interface INote {
+    id: number;
+    title: string;
+    content: string;
+    createdDate: Date;
+    editedDate: Date;
+    status: boolean;
+    requireConfirmation: boolean;
+}
+
+interface DefaultNote extends INote {
+    requireConfirmation: false;
+}
+
+interface ConfirmedNote extends INote {
+    requireConfirmation: true;
+}
+
+type NoteType = DefaultNote | ConfirmedNote;
+
+interface ITodoList {
+    addNote(title: string, content: string, requireConfirmation?: boolean): void;
+    removeNote(id: number): void;
+    editNote(id: number, title: string, content: string): void;
+    markAsDone(id: number): void;
+    getNoteById(id: number): NoteType | undefined;
+    getAllNotes(): NoteType[];
+    getUnfinishedNotesCount(): number;
+    getFinishedNotesCount(): number;
+    searchNotes(query: string): NoteType[];
+    sortByStatus(): void;
+    sortByCreationDate(): void;
+}
+
+class TodoListImplementation implements ITodoList {
+    private notes: NoteType[];
+
+    constructor() {
+        this.notes = [];
+    }
+
+    addNote(title: string, content: string, requireConfirmation: boolean = false): void {
+        if (title.trim() === '' || content.trim() === '') {
+            console.error('Title and content cannot be empty.');
+            return;
+        }
+
+        const newNote: NoteType = {
+            id: this.notes.length + 1,
+            title,
+            content,
+            createdDate: new Date(),
+            editedDate: new Date(),
+            status: false,
+            requireConfirmation,
+        };
+
+        this.notes.push(newNote);
+    }
+
+    removeNote(id: number): void {
+        this.notes = this.notes.filter((note) => note.id !== id);
+    }
+
+    editNote(id: number, title: string, content: string): void {
+        const noteIndex = this.findNoteIndexById(id);
+        if (noteIndex !== -1) {
+            this.notes[noteIndex].title = title;
+            this.notes[noteIndex].content = content;
+            this.notes[noteIndex].editedDate = new Date();
+            if (this.notes[noteIndex].requireConfirmation) {
+                this.notes[noteIndex].status = false;
+            }
         } else {
-            const result = originalMethod.apply(this, args);
-            memoizeMap.set(memoizeKey, result);
-            console.log('Method executed:', result);
+            console.error('Note not found.');
         }
-    };
+    }
 
-    return descriptor;
-}
+    markAsDone(id: number): void {
+        const noteIndex = this.findNoteIndexById(id);
+        if (noteIndex !== -1) {
+            this.notes[noteIndex].status = true;
+        } else {
+            console.error('Note not found.');
+        }
+    }
 
-class MemoizeClass {
-    @Memoize
-    calculate(arg1: number, arg2: string): string {
-        return `Result: ${arg1} - ${arg2}`;
+    getNoteById(id: number): NoteType | undefined {
+        return this.notes.find((note) => note.id === id);
+    }
+
+    getAllNotes(): NoteType[] {
+        return this.notes;
+    }
+
+    getUnfinishedNotesCount(): number {
+        return this.notes.filter((note) => !note.status).length;
+    }
+
+    getFinishedNotesCount(): number {
+        return this.notes.filter((note) => note.status).length;
+    }
+
+    searchNotes(query: string): NoteType[] {
+        const lowerCaseQuery = query.toLowerCase();
+        return this.notes.filter(
+            (note) =>
+                note.title.toLowerCase().includes(lowerCaseQuery) ||
+                note.content.toLowerCase().includes(lowerCaseQuery)
+        );
+    }
+
+    sortByStatus(): void {
+        this.notes.sort((a, b) => (a.status === b.status ? 0 : a.status ? 1 : -1));
+    }
+
+    sortByCreationDate(): void {
+        this.notes.sort((a, b) => a.createdDate.getTime() - b.createdDate.getTime());
+    }
+
+    private findNoteIndexById(id: number): number {
+        return this.notes.findIndex((note) => note.id === id);
     }
 }
 
-const instanceMemoize = new MemoizeClass();
+const todoList = new TodoListImplementation();
 
-function Debounce(wait: number) {
-    return function (_target: any, _key: string, descriptor: PropertyDescriptor) {
-      const originalMethod = descriptor.value!;
-      let timeout: number | undefined;
-  
-      descriptor.value = function (this: any, ...args: any[]) {
-        if (timeout !== undefined) {
-          clearTimeout(timeout);
-        }
-  
-        timeout = window.setTimeout(() => {
-          originalMethod.apply(this, args);
-        }, wait);
-      };
-  
-      return descriptor;
-    };
-  }
-  
-  class DebounceClass {
-    @Debounce(500)
-    methodWithDebounce(arg: string) {
-      console.log(`Method executed with argument: ${arg}`);
-    }
-  }
-  
-  const instanceDebounce = new DebounceClass();
-  
+// example of use
+
+todoList.addNote('Task1', 'Content1');
+todoList.addNote('Task2', 'Content2', true);
+todoList.editNote(1, 'Task 1 Updated', 'Updated Content 1');
+todoList.markAsDone(1);
+todoList.removeNote(2);
+const allNotes = todoList.getAllNotes();
+console.log(allNotes);
+const unfinishedCount = todoList.getUnfinishedNotesCount();
+const finishedCount = todoList.getFinishedNotesCount();
+console.log(`Unfinished Notes: ${unfinishedCount}, Finished Notes: ${finishedCount}`);
+const searchResults = todoList.searchNotes('Updated');
+console.log('Search Results:', searchResults);
+todoList
+
+
